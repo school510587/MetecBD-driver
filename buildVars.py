ddc2919c2f10423f4e0a9ccfb36bd13e3a78e5a5
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2020-06-01 06:39:07 +0800

    buildVars.py: Update author information

--- buildVars.py
+++ buildVars.py
@@ -21,7 +21,7 @@ addon_info = {
 	# version
 	"addon_version" : "1.1",
 	# Author(s)
-	"addon_author" : u"宇崝企業有限公司發行",
+	"addon_author" : u"宇崝企業有限公司發行，加上 Bo-Cheng Jhan <school510587@yahoo.com.tw> 的改進",
 	# URL for the add-on documentation support
 	"addon_url" : "http://www.u-tran.com/",
 	# Documentation file name
