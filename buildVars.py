88101eb28d101616c959f071882906bb32fddec0
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2020-10-30 14:35:23 +0800

    buildVars.py: Bump addon version to 1.2.2020.3
    From version 1.2, the version number follows this format:
    [Main Ver. #].[The last supported/tested NVDA Ver. #]

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Driver for MetecBD braille display."""),
 	# version
-	"addon_version" : "1.1",
+	"addon_version" : "1.2.2020.3",
 	# Author(s)
 	"addon_author" : u"Bo-Cheng Jhan <school510587@yahoo.com.tw> 改進自宇崝企業有限公司發行之舊版",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0")
 	"addon_minimumNVDAVersion" : "2017.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2020.1",
+	"addon_lastTestedNVDAVersion" : "2020.3",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
